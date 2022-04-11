b457404850564b955bf0e04714a782f18fb0a07e
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-04-11 22:22:47 +0300

    ci

---
+++ UniqueList/UniqueList/List.cs
@@ -0,0 +1,16 @@
+﻿namespace UniqueList;
+
+public class MyList<T>
+{
+    public MyList()
+    {
+        root = new Node<T>();
+    }
+
+    private Node<T> root;
+
+    public void Add(T value)
+    {
+
+    }
+}
---
+++ UniqueList/UniqueList/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace UniqueList;
+
+internal class Node<T>
+{
+    public Node()
+    {
+    }
+
+    public Node(T? value, Node<T>? next)
+        : this()
+    {
+        Value = value;
+        Next = next;
+    }
+
+    public T? Value { get; set; }
+
+    public Node<T>? Next { get; set; }
+}
+
